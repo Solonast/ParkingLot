55973333a33611a35bdc11cfa41631149f1288e9
Solonas Astraios <solonas.astraios@barclays.com>
2023-09-28 14:11:09 +0100

    updated

--- src/main/java/parkinglotapp/ParkingLotApp.java
+++ src/main/java/parkinglotapp/ParkingLotApp.java
@@ -3,10 +3,6 @@ package parkinglotapp;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.time.temporal.ChronoUnit;
 
 public class ParkingLotApp extends Application {
 
